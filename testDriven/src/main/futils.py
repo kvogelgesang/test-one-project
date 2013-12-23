93fdd3dec1a07afdc96ca64d609e2351ef40474b
kvogelgesang <kay.vogelgesang@apachefriends.org>
2013-12-23 16:21:22 +0100

    adding some fileUtils methods here

--- testDriven/src/main/futils.py
+++ testDriven/src/main/futils.py
@@ -10,6 +10,19 @@ class Futils:
     def get_resource_dir(self):
         return "%s/resources" % os.getcwd()
 
+    def is_readable(self,dir):
+        if os.access(dir, os.R_OK):
+            return True
+        else:
+            return False
+
+    def is_writeable(self,dir):
+        if os.access(dir, os.W_OK):
+            return True
+        else:
+            return False
+
+
 #mytest=Futils()
 #print mytest.get_base_dir()
 
--- testDriven/src/test/futilsTest.py
+++ testDriven/src/test/futilsTest.py
@@ -11,9 +11,18 @@ class FutilsTests(unittest.TestCase):
     global RESOURCE_DIR
     RESOURCE_DIR = "%s/resources" % os.getcwd()
 
-    def testnewestversion(self):
+    def testGetResourceDir(self):
         mytest=futils.Futils()
-        self.assertEquals(mytest.get_resource_dir(),RESOURCE_DIR, "test failed!")
+        self.assertEquals(mytest.get_resource_dir(), RESOURCE_DIR, "testGetResourceDir failed!")
+
+    def testIsReadable(self):
+        mytest=futils.Futils()
+        self.assertEquals(mytest.is_readable(RESOURCE_DIR) ,True, "testIsReadable failed!")
+
+    def testIsWriteable(self):
+        mytest=futils.Futils()
+        self.assertEquals(mytest.is_writeable(RESOURCE_DIR) ,True, "testIsWriteable failed!")
+
 
 
 if __name__ == '__main__':
